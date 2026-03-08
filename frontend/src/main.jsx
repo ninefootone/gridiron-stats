2d831223cdb580910e90c9c5660e70088b68181e
Jon Bradley <jon@MacBookPro.lan>
2026-03-08 09:20:54 +0000

    Fix redirect URI

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -11,7 +11,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
       domain={import.meta.env.VITE_AUTH0_DOMAIN}
       clientId={import.meta.env.VITE_AUTH0_CLIENT_ID}
       authorizationParams={{
-        redirect_uri: window.location.origin,
+        redirect_uri: window.location.origin + "/callback",
         audience: import.meta.env.VITE_AUTH0_AUDIENCE,
         scope: 'openid profile email',
       }}
