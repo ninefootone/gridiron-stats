23e0b4964421ec724afbb89095a2a38c1231af48
Jon Bradley <jon@MacBookPro.lan>
2026-04-18 22:15:59 +0100

    Replace Unicode upload arrow with SVG icon on import CSV button

--- frontend/src/pages/TeamDetailPage.jsx
+++ frontend/src/pages/TeamDetailPage.jsx
@@ -935,7 +935,8 @@ async function loadMembers() {
                 setImportModal(true);
               }}
             >
-              ⬆ Import from CSV
+              <svg xmlns="http://www.w3.org/2000/svg" width="15" height="15" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" style={{ marginRight: 6, verticalAlign: 'middle', position: 'relative', top: -1 }}><path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4"/><polyline points="17 8 12 3 7 8"/><line x1="12" y1="3" x2="12" y2="15"/></svg>
+              Import from CSV
             </button>
           </div>
         </Modal>
