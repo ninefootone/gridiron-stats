2a79b4372f7173191f7824a6dd83af3f200c395c
Jon Bradley <jon@MacBookPro.lan>
2026-04-19 19:06:08 +0100

    Fix catch-all route swallowing /drills

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import { Routes, Route, Navigate } from 'react-router-dom';
 import Layout from './components/layout/Layout';
 import LoginPage from './pages/LoginPage';
 import TeamsPage from './pages/TeamsPage';
+import DrillsPage from './pages/DrillsPage';
 import AdminPage from './pages/AdminPage';
 import TeamDetailPage from './pages/TeamDetailPage';
 import GamePage from './pages/GamePage';
@@ -30,14 +31,15 @@ export default function App() {
         <Route path="/" element={<PrivateRoute><Layout /></PrivateRoute>}>
           <Route index element={<Navigate to="/teams" replace />} />
           <Route path="teams" element={<TeamsPage />} />
+          <Route path="drills" element={<DrillsPage />} />
           <Route path="teams/:teamId" element={<TeamDetailPage />} />
           <Route path="teams/:teamId/games/:gameId" element={<GamePage />} />
           <Route path="teams/:teamId/players/:playerId" element={<PlayerPage />} />
           <Route path="teams/:teamId/leaderboard" element={<LeaderboardPage />} />
           <Route path="admin" element={<AdminPage />} />
+          <Route path="*" element={<Navigate to="/teams" replace />} />
         </Route>
         <Route path="/live/:gameId" element={<LivePage />} />
-          <Route path="*" element={<Navigate to="/teams" replace />} />
       </Routes>
       <CookieBanner />
       <OfflineBanner />
